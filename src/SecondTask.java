ea3f67a2e0893e7939c2312846735c480aa4e323
PoKKu56 <glazundev@gmail.com>
2024-04-09 12:17:38 +0500

    Выполнил задание 4

--- src/SecondTask.java
+++ src/SecondTask.java
@@ -1,18 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Collections;
 
 public class SecondTask {
-    public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        System.out.print("Введите число X: ");
-        Integer X = in.nextInt();
-        System.out.print("Введите длину массива: ");
-        Integer lenght = in.nextInt();
-        for (int i = 0; i < lenght; i++){
-            System.out.print("Введите элемент массива: ");
-            arrayList.add(in.nextInt());
+    public static void main(String[] args) {
+        String fileName = ""; // Указать путь к файлу
+        ArrayList<String> lines = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            System.out.println(e);
         }
-
+        Collections.sort(lines);
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
---
+++ src/ThirdTask.java
@@ -0,0 +1,9 @@
+public class ThirdTask {
+    public static void main(String[] args){
+
+    }
+
+    public static void getLoginPassword(String login, String password, String confirmPassword){
+
+    }
+}
